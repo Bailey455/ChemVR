84adc0b82d8ed9ea0f7a28e3a509c3c1f54ed78d
Bailey455 <90922108+Bailey455@users.noreply.github.com>
2022-10-03 20:46:36 -0500

    raycast_test_2

--- Assets/raycast.cs
+++ Assets/raycast.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class raycast : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
         
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right) * 250 * Time.deltaTime, out hit, 1000))
+        {
+            Debug.DrawRay(this.transform.position, this.transform.TransformDirection(Vector3.right) * 250 * Time.deltaTime, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * 250 * Time.deltaTime, Color.white);
+            Debug.Log("Did not Hit");
+        }
     }
 }
