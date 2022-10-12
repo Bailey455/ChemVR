bcda040067667aad894ea415f8b902cd98a949b2
Matthew Lad <90922108+Bailey455@users.noreply.github.com>
2022-10-12 10:55:56 -0500

    raycast_fix

--- Assets/raycast.cs
+++ Assets/raycast.cs
@@ -10,16 +10,27 @@ public class raycast : MonoBehaviour
         RaycastHit hit;
         
 
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right) * 250 * Time.deltaTime, out hit, 1000))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward) * 500 * Time.deltaTime, out hit))
         {
-            Debug.DrawRay(this.transform.position, this.transform.TransformDirection(Vector3.right) * 250 * Time.deltaTime, Color.yellow);
-            Debug.Log("Did Hit");
+            //Debug.Log(hit);
+            Debug.DrawRay(this.transform.position, this.transform.TransformDirection(Vector3.forward) * 500 * Time.deltaTime, Color.yellow);
+
+            if (hit.collider.CompareTag("Element"))
+            {
+                Debug.Log("Hit Element");
+            }
+
+            else if (hit.collider.CompareTag("Wall"))
+            {
+                Debug.Log("Did Wall");
+            }
+
         }
 
         else
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * 250 * Time.deltaTime, Color.white);
-            Debug.Log("Did not Hit");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 500 * Time.deltaTime, Color.white, 1000);
+            //Debug.Log("Did not Hit");
         }
     }
 }
