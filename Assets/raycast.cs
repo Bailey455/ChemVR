93ff643fdfc3fdffa9846b4d9a7f5c24e63f8492
Bailey455 <90922108+Bailey455@users.noreply.github.com>
2022-10-19 13:53:22 -0500

    fix

--- Assets/raycast.cs
+++ Assets/raycast.cs
@@ -1,58 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-using UnityEngine.SceneManagement;
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+using TMPro; 
+using UnityEngine.SceneManagement; 
 
-public class raycast : MonoBehaviour
+public class raycast : MonoBehaviour 
 {
-    public GameObject test;
-    public GameObject look;
-    public GameObject elements;
+    public GameObject test; 
+    public GameObject look; 
+    public GameObject elements; 
 
+    // Update is called once per frame 
+    void Update() 
+    { 
+        RaycastHit hit;  
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward) * 500 * Time.deltaTime, out hit, 1000)) 
+        { 
+            Debug.DrawRay(this.transform.position, this.transform.TransformDirection(Vector3.forward) * 500 * Time.deltaTime, Color.yellow); 
+            //Debug.Log("Did Hit"); 
 
-    // Update is called once per frame
-    void Update()
-    {
-        RaycastHit hit; 
+            if (hit.collider.gameObject.CompareTag("Element")) 
+            { 
+                Debug.Log("Hit Element"); 
 
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward) * 500 * Time.deltaTime, out hit, 1000))
-        {
-            Debug.DrawRay(this.transform.position, this.transform.TransformDirection(Vector3.forward) * 500 * Time.deltaTime, Color.yellow);
-            //Debug.Log("Did Hit");
+                if (OVRInput.Get(OVRInput.Button.One)) 
+                { 
+                    Debug.Log("Pressed A"); 
+                    test.SetActive(true); 
+                    look.SetActive(true); 
+                    elements.SetActive(false); 
+                } 
 
-            if (hit.collider.gameObject.CompareTag("Element"))
+            } 
+
+            if (hit.collider.gameObject.CompareTag("continueButton")) 
+
+                    { 
+
+                        if(OVRInput.Get(OVRInput.Button.One)) 
+
+                        { 
+
+                            look.SetActive(false); 
+
+                            test.SetActive(false); 
+
+                            elements.SetActive(true); 
+
+                        } 
+
+                    } 
+
+ 
+ 
+
+            if (hit.collider.gameObject.CompareTag("group1") && OVRInput.Get(OVRInput.Button.One)) 
+
+            { 
+
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+
+            } 
+           
+            if (hit.collider.gameObject.CompareTag("back") && OVRInput.Get(OVRInput.Button.One)) 
             {
-                Debug.Log("Hit Element");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); 
+            } 
 
-                if (OVRInput.Get(OVRInput.Button.One))
-                {
-                    Debug.Log("Pressed A");
-                    test.SetActive(true);
-                    look.SetActive(true);
-                    elements.SetActive(false);
-                }
-            }
+        } 
 
-            if (hit.collider.gameObject.CompareTag("continueButton"))
-                    {
-                        if(OVRInput.Get(OVRInput.Button.One))
-                        {
-                            look.SetActive(false);
-                            test.SetActive(false);
-                            elements.SetActive(true);
-                        }
-                    }
+    } 
 
-            if (hit.collider.gameObject.CompareTag("group1") && OVRInput.Get(OVRInput.Button.One))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            }
+} 
 
-            if (hit.collider.gameObject.CompareTag("back") && OVRInput.Get(OVRInput.Button.One))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-            }
-        }
-    }
-}
+ 
+ 
+
+ 
